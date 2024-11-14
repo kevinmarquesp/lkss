2d04e73a8da49f4048eb3ef0979973504cf2a460
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-11-13 23:32:13 -0300

    :bug: fix (hooks): Removed the delay function that was there just for testing

--- hooks/use-shortner.ts
+++ hooks/use-shortner.ts
@@ -1,7 +1,5 @@
 import { useState } from "react";
 
-const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
-
 function useShortner<T>() {
   const [loading, setLoading] = useState(false);
   const [result, setResult] = useState<T | null>(null);
@@ -10,8 +8,6 @@ function useShortner<T>() {
 
   async function shorten(url: string) {
     setLoading(true);
-
-    await delay(3_000);
 
     const response = await fetch("/api/v1/create", {
       method: "POST",
